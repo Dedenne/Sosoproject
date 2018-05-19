60ec7a9788adcfd7b696b8df1ab5196be750426a
Dedenne <r.chedru@icloud.com>
2018-05-19 13:37:52 +0100

    counts stone for player and AI

--- Artificial.py
+++ Artificial.py
@@ -24,3 +24,29 @@ class Game:
 		if ny.shape( ny.where( self.board == STONE_BLANK ) )[1] == 0:
 			return True
 		return False
+
+	def _count_stones( self, column, row, stone, blank, ai, human ):
+		""" Compares the value of the found stone with the existing stone types.
+		Sets a stop flag if it is not possible anymore for either the human or the AI
+		to connect enough stones.
+		Returns a tuple in the form: ( position of a blank field, #ai, #human, flag_to_stop ).
+		"""
+
+		flag_stop = False
+
+		if stone == 0:
+			# No danger/chance if there is more than one blank field in range
+			if blank != -1: flag_stop = True
+			# Save blank field. This is a candidate to place the stone.
+			else: blank = { "col": column, "row": row }
+		elif stone == -1:
+			# If there has been at least one human stone already,
+			# it is not possible for AI stones to have a connection of three and a blank field available.
+			if human > 0: flag_stop = True
+			else: ai += 1
+		elif stone == 1:
+			# Same here, vice versa.
+			if ai > 0: flag_stop = True
+			else: human += 1
+
+		return ( blank, ai, human, flag_stop )
